329d8be5e4669a58b71513e13c968bc4719e24d9
CGDM101 <51097129+CGDM101@users.noreply.github.com>
2021-01-17 23:53:05 +0100

    Add project files.

---
+++ TodoItem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Todolist
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
